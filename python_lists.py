7081cfcd20c23a78b1d32ae0e169cd69dbe569f9
Kwabena Sarkodie <sarkodiekwabena870@gmail.com>
2025-06-08 12:14:03 +0100

    lists

---
+++ python_lists.py
@@ -0,0 +1,29 @@
+#listsm
+bicycles = ["trek", "cannondale","redline" ]
+print(bicycles)
+
+cycles = ['trek', 'cannondale' , 'redline']
+print(cycles[1])
+
+cycles = ['trek', 'cannondale' , 'redline']
+print(cycles[0].title())
+
+names = ['Sark','Emma','James','Nicholas']
+print(names[1])
+
+motorcycles = ['Suzuki','Yahama','Honda']
+print(motorcycles)
+
+motorcycles[0] = 'ducti'
+print(motorcycles)
+
+motorcycles = ['Suzuki','Yahama','Honda']
+print(motorcycles)
+
+motorcycles.append('ducti')
+print(motorcycles)
+
+motorcycles = ['Suzuki','Yahama','Honda']
+
+motorcycles.insert(0,'ducati')
+print(motorcycles)
