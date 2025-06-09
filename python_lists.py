28094c159da99b5537f0ced240b53467c41e9edd
Kwabena Sarkodie <sarkodiekwabena870@gmail.com>
2025-06-09 21:59:30 +0100

    tuple

--- python_lists.py
+++ python_lists.py
@@ -27,3 +27,28 @@ motorcycles = ['Suzuki','Yahama','Honda']
 
 motorcycles.insert(0,'ducati')
 print(motorcycles)
+
+magicians = ['Alice','carloina','dsvid']
+for magician in magicians:
+  print(magician)
+
+  cats = ('black','white','red')
+  for cat in cats:
+    print(cat)
+#numerical lists
+for value in range(1,9):
+    print(value)   
+
+numbers = list(range(1,11))
+print(numbers)
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
+dimensions = (200, 50)
+print(dimensions[1])
+
+dimensions = (200, 50)
+dimensions[0] = 250
