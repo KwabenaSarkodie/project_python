f5e0654895a7337783df36c2ac644e47c360e80b
Kwabena Sarkodie <sarkodiekwabena870@gmail.com>
2025-06-16 09:04:00 +0100

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+chief_0 = {'color':'green', 'points':5}
+print(chief_0)
+
+del chief_0['points']
+print(chief_0)
