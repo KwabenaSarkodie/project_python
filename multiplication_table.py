210c34e672c6db69ad0772ea4939b4cb5bb80a19
Kwabena Sarkodie <sarkodiekwabena870@gmail.com>
2025-07-23 22:29:10 +0100

    code_for_a_multiplication_table_from_1-12

---
+++ multiplication_table.py
@@ -0,0 +1,24 @@
+for i in range(1,13):
+    print(i)
+for i in range(2,25,2):
+    print(i)
+for i in range(3,36,3):
+    print(i)
+for i in range(4,49,4):
+    print(i)
+for i in range(5,61,5):
+    print(i)
+for i in range(6,73,6):
+    print(i)
+for i in range(7,84,7):
+    print(i)
+for i in range(8,97,8):
+    print(i)
+for i in range(9,109,9):
+    print(i)
+for i in range(10,121,10):
+    print(i)
+for i in range(11,121,11):
+    print(i)
+for i in range(12,145,12):
+    print(i)
