3538e801211f695b0ad7b3c081967c9d241d9a10
Kwabena Sarkodie <sarkodiekwabena870@gmail.com>
2025-07-27 00:40:47 +0100

    functions

--- functions.py
+++ functions.py
@@ -17,4 +17,30 @@ def describe_pet(animal_type, pet_name):
 
 describe_pet('hamster', 'harry')
 describe_pet('dog','bingo')
- 
+
+def get_formatted_name(first_name,last_name):
+     "Return a full name,formatted well"
+     full_name = first_name + ' ' + last_name
+     return full_name.title()
+musician = get_formatted_name('john','cena')
+print(musician)
+
+def get_city_country(city,country):
+     "Return a city with its country of location"
+     location_name = city + ',' + country
+     return location_name.title()
+dancer = get_city_country("santiago","chile")
+print(dancer)
+
+def make_album(artist_name, album_name ,tracks=None):
+    "Return a dictionary for album."
+    person = {'artist': artist_name , 'album':album_name}
+    if tracks:
+         album_name['tracks'] = tracks
+    return person
+musician = make_album('juice wrld', 'life goes on')
+print(musician)
+
+#creating three albums
+print(make_album("Forrest Frank", "UP"))
+print(make_album("Moses Bliss","Darling Jesus"))
